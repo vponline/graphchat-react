087ef9f8e8a779102e166c0925161eaccf8f78de
vp <vepant@gmail.com>
2020-09-26 16:45:35 +0800

    setup sequelize

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-const { ApolloServer } = require('apollo-server')
-const resolvers = require('./graphql/resolvers/index')
-const typeDefs = require('./graphql/typedefs')
+const {
+  ApolloServer
+} = require("apollo-server")
+const {
+  sequelize
+} = require("./models/index")
+const resolvers = require("./graphql/resolvers/index")
+const typeDefs = require("./graphql/typedefs")
+const authContext = require("./middleware/auth")
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
-});
+  resolvers,
+  context: authContext,
+})
 
-server.listen().then(({ port }) => {
-  console.log(`Server running on port ${port}`);
-});
+server.listen().then(({
+  port
+}) => {
+  console.log(`Server running on port ${port}`)
+
+  sequelize
+    .authenticate()
+    .then(() => console.log("Database connected"))
+    .catch((err) => console.log(err))
+})
