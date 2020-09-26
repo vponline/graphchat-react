e5f54f0a26f868ec1c28fb70b47f93b08edcf085
vp <vepant@gmail.com>
2020-09-26 16:24:46 +0800

    create graphql typedefs and resolvers

---
+++ graphql/resolvers/messages.js
@@ -0,0 +1,228 @@
+const {
+  UserInputError,
+  AuthenticationError,
+  ForbiddenError,
+  withFilter,
+} = require("apollo-server")
+const {
+  Op
+} = require("sequelize")
+const {
+  User,
+  Message,
+  Reaction
+} = require("../../models/index")
+
+module.exports = {
+  //{ from } comes from args, { user } comes from context
+  Query: {
+    getMessages: async (parent, {
+      from
+    }, {
+      user
+    }) => {
+      try {
+        if (!user) throw new AuthenticationError("Unauthenticated")
+
+        //get the selected user
+        const otherUser = await User.findOne({
+          where: {
+            username: from,
+          },
+        })
+
+        if (!otherUser) throw new UserInputError("User not found")
+
+        const usernames = [user.username, otherUser.username]
+
+        //get messages for both chat participants
+        const messages = await Message.findAll({
+          where: {
+            from: {
+              [Op.in]: usernames,
+            },
+            to: {
+              [Op.in]: usernames,
+            },
+          },
+          order: [
+            ["createdAt", "DESC"]
+          ],
+          include: [{
+            model: Reaction,
+            as: "reactions",
+          }, ],
+        })
+
+        return messages
+      } catch (err) {
+        console.log(err)
+        throw err
+      }
+    },
+  },
+  Mutation: {
+    //{ to, content } come from args.to and args.content
+    sendMessage: async (parent, {
+      to,
+      content
+    }, {
+      user,
+      pubsub
+    }) => {
+      try {
+        if (!user) throw new AuthenticationError("Unauthenticated")
+
+        //set the selected user as the recipient
+        const recipient = await User.findOne({
+          where: {
+            username: to,
+          },
+        })
+        if (!recipient) {
+          throw new UserInputError("User not found")
+        }
+        //Prevent users from messaging themselves
+        // else if (recipient.username === user.username) {
+        //   throw new UserInputError("Cannot message self")
+        // }
+
+        if (content.trim() === "") {
+          throw new UserInputError("Message content is required")
+        }
+
+        //create new message
+        const message = await Message.create({
+          from: user.username,
+          to: to,
+          content: content,
+        })
+
+        //publish new message to websocket
+        pubsub.publish("NEW_MESSAGE", {
+          newMessage: message,
+        })
+
+        return message
+      } catch (err) {
+        console.log(err)
+        throw err
+      }
+    },
+    // { uuid, content } come from args, { user, pubsub } come from context
+    reactToMessage: async (parent, {
+      uuid,
+      content
+    }, {
+      user,
+      pubsub
+    }) => {
+      const reactions = ["👍", "👎", "🙄", "🥶", "☠", "🍝", "🍦", "🍩", "😢", "😆", "😯", "❤️", "😡"]
+
+      try {
+        //throw error if no reaction
+        if (!reactions.includes(content)) {
+          throw new UserInputError("Invalid reaction")
+        }
+
+        const username = user ? user.username : ""
+        user = await User.findOne({
+          where: {
+            username,
+          },
+        })
+        if (!user) throw new AuthenticationError("Unauthenticated")
+
+        const message = await Message.findOne({
+          where: {
+            uuid,
+          },
+        })
+        if (!message) throw new UserInputError("Message not found")
+
+        if (message.from !== user.username && message.to !== user.username) {
+          throw new ForbiddenError("Unauthorized")
+        }
+
+        let reaction = await Reaction.findOne({
+          where: {
+            messageId: message.id,
+            userId: user.id,
+          },
+        })
+
+        if (reaction) {
+          //update and save if a reaction already exists
+          reaction.content = content
+          await reaction.save()
+        } else {
+          //create new reaction if no reaction
+          reaction = await Reaction.create({
+            messageId: message.id,
+            userId: user.id,
+            content: content,
+          })
+        }
+
+        pubsub.publish("NEW_REACTION", {
+          newReaction: reaction,
+        })
+
+        return reaction
+      } catch (err) {
+        throw err
+      }
+    },
+  },
+  //setup graphql subscriptions for messages and reactions
+  Subscription: {
+    newMessage: {
+      subscribe: withFilter(
+        (parent, args, {
+          pubsub,
+          user
+        }) => {
+          if (!user) throw new AuthenticationError("Unauthenticated")
+          return pubsub.asyncIterator("NEW_MESSAGE")
+        },
+        ({
+          newMessage
+        }, _, {
+          user
+        }) => {
+          if (
+            newMessage.from === user.username ||
+            newMessage.to === user.username
+          ) {
+            return true
+          }
+
+          return false
+        }
+      ),
+    },
+    newReaction: {
+      subscribe: withFilter(
+        (parent, args, {
+          pubsub,
+          user
+        }) => {
+          if (!user) throw new AuthenticationError("Unauthenticated")
+          return pubsub.asyncIterator("NEW_REACTION")
+        },
+        async ({
+          newReaction
+        }, _, {
+          user
+        }) => {
+          const message = await newReaction.getMessage()
+          if (message.from === user.username || message.to === user.username) {
+            return true
+          }
+
+          return false
+        }
+      ),
+    },
+  },
+}
---
+++ graphql/resolvers/users.js
@@ -0,0 +1,188 @@
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const {
+  UserInputError,
+  AuthenticationError
+} = require("apollo-server")
+const {
+  Op
+} = require("sequelize")
+const {
+  JWT_SECRET
+} = require("../../config/env.json")
+const {
+  User,
+  Message
+} = require("../../models/index")
+
+module.exports = {
+  Query: {
+    //{ user } is coming from context.user
+    getUsers: async (parent, args, {
+      user
+    }) => {
+      try {
+        if (!user) throw new AuthenticationError("Unauthenticated")
+        //get all users from database
+        let users = await User.findAll({
+          attributes: ["username", "imageUrl", "createdAt"],
+          where: {
+            username: {
+              [Op.ne]: user.username,
+            },
+          },
+        })
+
+        //get messages for each user
+        const allUserMessages = await Message.findAll({
+          where: {
+            [Op.or]: [{
+                from: user.username,
+              },
+              {
+                to: user.username,
+              },
+            ],
+          },
+          order: [
+            ["createdAt", "DESC"]
+          ],
+        })
+
+        //find the latest message for each user
+        users = users.map((otherUser) => {
+          const latestMessage = allUserMessages.find(
+            (m) => m.from === otherUser.username || m.to === otherUser.username
+          )
+          otherUser.latestMessage = latestMessage
+          return otherUser
+        })
+
+        return users
+      } catch (err) {
+        console.log(err)
+        throw err
+      }
+    },
+    login: async (parent, args) => {
+      const {
+        username,
+        password
+      } = args
+      let errors = {}
+
+      try {
+        if (username.trim() === "")
+          errors.username = "Username is required"
+        if (password === "") errors.password = "Password is required"
+
+        //throw error if there are any errors in the errors object
+        if (Object.keys(errors).length > 0) {
+          throw new UserInputError("Bad input", {
+            errors,
+          })
+        }
+
+        const user = await User.findOne({
+          where: {
+            username: username,
+          },
+        })
+
+        if (!user) {
+          errors.username = "User not found"
+          throw new UserInputError("User not found", {
+            errors,
+          })
+        }
+
+        const correctPassword = await bcrypt.compare(password, user.password)
+
+        if (!correctPassword) {
+          errors.password = "Password is incorrect"
+          throw new UserInputError("Password is incorrect", {
+            errors,
+          })
+        }
+
+        const token = jwt.sign({
+            username: username,
+          },
+          JWT_SECRET, {
+            expiresIn: 60 * 60,
+          }
+        )
+
+        return {
+          ...user.toJSON(),
+          token,
+        }
+      } catch (err) {
+        console.log(err)
+        throw err
+      }
+    },
+  },
+  Mutation: {
+    register: async (parent, args, context, info) => {
+      let {
+        username,
+        email,
+        password,
+        confirmPassword
+      } = args
+      let errors = {}
+
+      try {
+        //validate input data
+        if (email.trim() === "") errors.email = "Email is required"
+        if (username.trim() === "")
+          errors.username = "Username is required"
+        if (password.trim() === "")
+          errors.password = "Password is required"
+        if (confirmPassword.trim() === "")
+          errors.confirmPassword = "Password confirmation is required"
+        if (password !== confirmPassword)
+          errors.confirmPassword = "Passwords must match"
+
+        // // check if username or email exists
+        // const userByUsername = await User.findOne({ where: { username: username } })
+        // const userByEmail = await User.findOne({ where: { email: email } })
+
+        // if(userByUsername) errors.username = 'username is taken'
+        // if(userByEmail) errors.email = 'email is taken'
+
+        if (Object.keys(errors).length > 0) {
+          throw errors
+        }
+
+        //hash password
+        password = await bcrypt.hash(password, 6)
+
+        //create user
+        const user = await User.create({
+          username,
+          email,
+          password,
+        })
+        //return user as json to the client
+        return user
+      } catch (err) {
+        console.log(err)
+        if (err.name === "SequelizeUniqueConstraintError") {
+          err.errors.forEach(
+            (e) =>
+            (errors[e.path.split("users.")[1]] = `${
+                e.path.split("users.")[1]
+              } is already taken`)
+          )
+        } else if (err.name === "SequelizeValidationError") {
+          err.errors.forEach((e) => (errors[e.path] = e.message))
+        }
+        throw new UserInputError("Bad input", {
+          errors,
+        })
+      }
+    },
+  },
+}
